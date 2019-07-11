b9550346b9e2f1ed9d8d53b9fb6ac24a32c44f97
fuyans <fuyans@gmail.com>
2019-07-11 08:01:56 +0100

    WIP mcs_gen

--- sfeprapy/mc0/mc0_func_gen_example.py
+++ sfeprapy/mc0/mc0_func_gen_example.py
@@ -54,3 +54,93 @@ def benchmark_0():
     df = pd.read_csv(StringIO(csv_), sep=",")
 
     print(df)
+
+
+def mcs_gen_test_muted():
+    from sfeprapy.func.mcs_gen import main
+    import numpy as np
+
+    dict_in = dict(
+        fire_mode=np.pi,
+        probability_weight=0.2,
+        n_simulations=10000,
+        time_step=30,
+        time_duration=18000,
+        fire_hrr_density=dict(
+            dist='uniform_',
+            lbound=0.25,
+            ubound=1.00),
+        fire_qfd=dict(
+            dist='gumbel_r_',
+            lbound=50,
+            ubound=2000,
+            mean=420,
+            sd=126),
+        fire_spread=dict(
+            dist='uniform_',
+            lbound=0.0035,
+            uboudn=0.019),
+        fire_nft_limit=dict(
+            dist='norm_',
+            lbound=293.15,
+            ubound=1673.15,
+            mean=1323.15,
+            sd=0),
+        fire_combustion_efficient(
+            dist='uniform_',
+        ),
+        fire_tlim=0.333,
+        fire_t_alpha=300,
+        fire_gamma_fi_q=1,
+        room_breadth=16,
+        room_depth=31.25,
+        room_height=3,
+        room_window_width=72,
+        room_window_height=2.8,
+        room_opening_fraction=dict(
+            dict='lognorm_mod_',
+        ),
+        room_opening_permanent_fraction=0,
+        room_wall_thermal_interia=720,
+        beam_cross_section_area=702,
+        #         v2='hello world.',
+        #         v3=dict(
+        #             dist='uniform_',
+        #             ubound=10,
+        #             lbound=-1
+        #         ),
+        #         v4=dict(
+        #             dist='norm_',
+        #             ubound=5+1,
+        #             lbound=5-1,
+        #             mean=5,
+        #             sd=1
+        #         ),
+        #         v5=dict(
+        #             dist='gumbel_r_',
+        #             ubound=2500,
+        #             lbound=50,
+        #             mean=420,
+        #             sd=126
+        #         ),
+        #         v6=dict(
+        #             dist='lognorm_',
+        #             ubound=1,
+        #             lbound=0,
+        #             mean=0.5,
+        #             sd=1,
+        #         ),
+        #         v7=dict(
+        #             dist='lognorm_mod_',
+        #             ubound=1,
+        #             lbound=0,
+        #             mean=0.5,
+        #             sd=1,
+        #         )
+    )
+
+    df_out = main(dict_in, 10000)
+
+    print(df_out.to_string(max_rows=10, max_cols=10, index=False))
+
+mcs_gen_test_muted()
