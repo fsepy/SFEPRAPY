869d16d42096edb2233710a7941a8ec4c370764f
fuyans <fuyans@gmail.com>
2019-07-23 01:26:47 +0100

    mcs0 initial

---
+++ sfeprapy/mcs0/__main__.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import warnings
+from sfeprapy.mc0.mc0 import main
+
+if __name__ == '__main__':
+    warnings.filterwarnings('ignore')
+    main()
