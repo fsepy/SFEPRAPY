669d8af38be9906f07a579e9d0568b8776f1f41f
fuyans <fuyans@gmail.com>
2019-09-10 23:09:26 +0100

    added sys.argv.

--- sfeprapy/mcs0/__main__.py
+++ sfeprapy/mcs0/__main__.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 
 if __name__ == '__main__':
+    import sys
     import warnings
     from sfeprapy.func.mcs_obj import MCS
     from sfeprapy.mcs0.mcs0_calc import teq_main, teq_main_wrapper, mcs_out_post
@@ -10,7 +11,24 @@ if __name__ == '__main__':
     warnings.filterwarnings('ignore')
 
     mcs = MCS()
-    mcs.define_problem()
+
+    if len(sys.argv) > 1:
+        mcs.define_problem(sys.argv[1])
+        for arg in sys.argv[2:]:
+            print(arg)
+            if 'sim' in arg:
+                print(arg)
+                n_simulations = int(str(arg).replace('sim', ''))
+                if mcs.config is None:
+                    print(mcs.config)
+
+                    mcs.config = dict(n_simulations=n_simulations)
+                else:
+                    mcs.config['n_simulations'] = int(str(arg).replace('sim', ''))
+                break
+    else:
+        mcs.define_problem()
+
     mcs.define_stochastic_parameter_generator(gen)
     mcs.define_calculation_routine(teq_main, teq_main_wrapper, mcs_out_post)
     mcs.run_mcs()
