5ff007dbd6fb83995fbcd88b29c8ad44cacec2e8
fuyans <fuyans@gmail.com>
2019-09-24 15:45:17 +0100

    depreciated `sfeprapy.mc0`. This module is imported/combined into `sfeprapy.mcs0` at version 0.6.

--- sfeprapy/mc0/__main__.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-
-import warnings
-from sfeprapy.mc0.mc0 import main
-
-if __name__ == '__main__':
-    warnings.filterwarnings('ignore')
-    main()
