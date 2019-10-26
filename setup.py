911887b62e0fc17c8e20f77dc180f48ffcdad9f7
fuyans <fuyans@gmail.com>
2019-10-26 15:05:02 +0100

    black style.

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ import sfeprapy
 with open(os.path.join(os.path.dirname(os.path.realpath(__file__)), "README.md")) as f:
     long_description = f.read()
 
-with open('requirements.txt') as f:
+with open("requirements.txt") as f:
     requirements = f.read().splitlines()
 
 setuptools.setup(
