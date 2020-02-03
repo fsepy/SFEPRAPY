4695aba420ff5b04f4f4da26bb3dc0d41712cb43
DannyHopkinFire <56392625+DannyHopkinFire@users.noreply.github.com>
2020-02-03 16:41:00 +0000

    Added test function for oversized vents

--- sfeprapy/mcs0/mcs0_calc.py
+++ sfeprapy/mcs0/mcs0_calc.py
@@ -437,7 +437,7 @@ def solve_time_equivalence(
                 ):  # Terminate if maximum solving iteration is reached
 
                     if solver_temperature_goal > y3:
-                        solver_time_equivalence_solved = -np.inf
+                        solver_time_equivalence_solved = 0.
                     elif solver_temperature_goal < y3:
                         solver_time_equivalence_solved = np.inf
                     else:
@@ -935,3 +935,50 @@ def _test_standard_case():
     print(teq_at_80_percentile)
     target, target_tol = 60, 2
     assert target - target_tol < teq_at_80_percentile < target + target_tol
+
+
+def _test_standard_oversized_case():
+    """the same as standard case but window_height and window_width are set to over the floor limit."""
+    import copy
+    from sfeprapy.func.mcs_obj import MCS
+    from sfeprapy.mcs0 import EXAMPLE_INPUT_DICT, EXAMPLE_CONFIG_DICT
+    # from sfeprapy.mcs0.mcs0_calc import teq_main, teq_main_wrapper, mcs_out_post
+    from sfeprapy.func.mcs_gen import main as gen
+    from scipy.interpolate import interp1d
+    import numpy as np
+
+    # increase the number of simulations so it gives sensible results
+    mcs_input = copy.deepcopy(EXAMPLE_INPUT_DICT)
+    mcs_config = copy.deepcopy(EXAMPLE_CONFIG_DICT)
+    for k in list(mcs_input.keys()):
+        mcs_input[k]["phi_teq"] = 1
+        mcs_input[k]["n_simulations"] = 10
+        mcs_input[k]["probability_weight"] = 1 / 3.0
+        mcs_input[k]["fire_time_duration"] = 10000
+        mcs_input[k]["timber_exposed_area"] = 0
+        mcs_input[k].pop("beam_position_horizontal")
+        mcs_input[k]["beam_position_horizontal:dist"] = "uniform_"
+        mcs_input[k]["beam_position_horizontal:ubound"] = (
+            mcs_input[k]["room_depth"] * 0.9
+        )
+        mcs_input[k]["beam_position_horizontal:lbound"] = (
+            mcs_input[k]["room_depth"] * 0.6
+        )
+        mcs_input[k]['window_width'] = 500
+        mcs_input[k]['window_height'] = 5
+
+    # increase the number of threads so it runs faster
+    mcs_config["n_threads"] = 1  # coverage does not support
+    mcs = MCS()
+    mcs.define_problem(data=mcs_input, config=mcs_config)
+    mcs.define_stochastic_parameter_generator(gen)
+    mcs.define_calculation_routine(teq_main, teq_main_wrapper, mcs_out_post)
+    mcs.run_mcs()
+    mcs_out = mcs.mcs_out
+    teq = mcs_out["solver_time_equivalence_solved"] / 60.0
+    hist, edges = np.histogram(teq, bins=np.arange(0, 181, 0.5))
+    x, y = (edges[:-1] + edges[1:]) / 2, np.cumsum(hist / np.sum(hist))
+    teq_at_80_percentile = interp1d(y, x)(0.8)
+
+    assert len(mcs_out['window_height']) == sum(mcs_out['window_height'].values == 3.2)
+    assert len(mcs_out['window_width']) == sum(mcs_out['window_width'].values == 94.5)
