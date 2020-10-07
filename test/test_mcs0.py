1695d8cb03db40286bedc558a74e4b9a56539415
fuyans <fuyans@gmail.com>
2020-10-07 22:23:58 +0100

    Combined `test_standard_case_timber` into `test_standard_case`.

--- test/test_mcs0.py
+++ test/test_mcs0.py
@@ -1,9 +1,7 @@
 # -*- coding: utf-8 -*-
 
 from sfeprapy.mcs0.mcs0_calc import _test_standard_case as test_standard_case
-from sfeprapy.mcs0.mcs0_calc import _test_standard_case_timber as test_standard_case_timber
 from sfeprapy.mcs0.mcs0_calc import _test_teq_phi as test_teq_phi
 
 test_teq_phi()
 test_standard_case()
-test_standard_case_timber()
