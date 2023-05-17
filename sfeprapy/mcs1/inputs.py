cc1e01baec2148493b1b399c4c2ede354c95d242
fuyans <fuyans@gmail.com>
2023-05-17 21:07:24 +0100

    add required variables for mcs1, these were missed in previous commits

--- sfeprapy/mcs1/inputs.py
+++ sfeprapy/mcs1/inputs.py
@@ -6,8 +6,9 @@ for i in (
         'solver_thickness_ubound', 'solver_tol'
 ):
     EXAMPLE_INPUT['CASE_1'].pop(i)
-EXAMPLE_INPUT['epsilon_q'] = dict(ubound=1 - 1e-9, lbound=1e-9, dist='uniform_')
-EXAMPLE_INPUT['t_k_y_theta'] = 5 * 60.
+EXAMPLE_INPUT['CASE_1']['epsilon_q'] = dict(ubound=1 - 1e-9, lbound=1e-9, dist='uniform_')
+EXAMPLE_INPUT['CASE_1']['t_k_y_theta'] = 5 * 60.
+EXAMPLE_INPUT['CASE_1']['protection_d_p'] = 0.01
 
 if __name__ == "__main__":
     import pprint
