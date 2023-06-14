e59ac5c799060567ca1c276eaee9ac1c3089239b
fuyans <fuyans@gmail.com>
2023-06-14 20:18:25 +0100

    update to align with latest mcs api

--- profiler/mcs0.py
+++ profiler/mcs0.py
@@ -3,14 +3,14 @@ import cProfile
 
 
 def profile_standard_case():
-    import copy
     from sfeprapy.mcs0 import MCS0, EXAMPLE_INPUT
 
     # increase the number of simulations so it gives sensible results
-    mcs_input = copy.deepcopy(EXAMPLE_INPUT)
+    mcs_input = dict()
+    mcs_input['CASE_1'] = EXAMPLE_INPUT['CASE_1'].copy()
     for k in list(mcs_input.keys()):
         mcs_input[k]["phi_teq"] = 1
-        mcs_input[k]["n_simulations"] = 1000
+        mcs_input[k]["n_simulations"] = 10_000
         mcs_input[k]["timber_exposed_area"] = 0
         mcs_input[k].pop("beam_position_horizontal")
         mcs_input[k]["beam_position_horizontal:dist"] = "uniform_"
@@ -18,9 +18,9 @@ def profile_standard_case():
         mcs_input[k]["beam_position_horizontal:lbound"] = mcs_input[k]["room_depth"] * 0.6
 
     # increase the number of threads so it runs faster
-    mcs = MCS0(1)
+    mcs = MCS0()
     mcs.set_inputs_dict(mcs_input)
-    mcs.run()
+    mcs.run(4, save=False)
 
 
 if __name__ == "__main__":
